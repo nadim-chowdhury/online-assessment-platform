ba1d23da3d68a3abc95421a7d4756021fa916bf4
nadim-chowdhury <nadim-chowdhury@outlook.com>
2026-04-10 04:38:10 +0600

    first commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Online Assessment Platform",
+  description:
+    "Online Assessment Platform with two panels: Employer Panel and Candidate Panel. The goal is to demonstrate frontend development skills, component design, state management, and handling of complex UI workflows",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        <TooltipProvider>{children}</TooltipProvider>
+      </body>
+    </html>
+  );
+}
